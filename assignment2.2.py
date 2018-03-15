486b3087ec88bd851e52f008485970214fe30898
vinay raj <vinayraj33@live.com>
2018-03-16 00:04:54 +0530

    assignment uploaded

---
+++ assignment2.2.py
@@ -0,0 +1,14 @@
+i = 1
+while i <= 5:
+    string = ''
+    for j in range(0,i):
+        string += '*'
+    print(string)
+    i = i + 1
+i = 4
+while i >= 1:
+    string = ''
+    for j in range(0,i):
+        string += '*'
+    print(string)
+    i = i - 1
